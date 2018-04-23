90654ad32b3a3cfc5b9ac3e24b8309d63d6c841c
Thiago Alves Pereira <thiago-alvesp@live.com>
2018-04-22 22:41:03 -0300

    Curso Finalizado.

--- Alura.LojaAPI/Controllers/CarrinhoController.cs
+++ Alura.LojaAPI/Controllers/CarrinhoController.cs
@@ -11,18 +11,64 @@ namespace Alura.LojaAPI.Controllers
 {
     public class CarrinhoController : ApiController
     {
-        public Carrinho Get(int id)
+        //[Route("api/carrinho/{idCarrinho}/produto/{idProduto}")]
+        //public HttpResponseMessage Put([FromBody]Produto produto, [FromUri] int idCarrinho, [FromUri] int idProduto)
+        //{
+        //    var dao = new CarrinhoDAO();
+        //    var carrinho = dao.Busca(idCarrinho);
+
+        //    carrinho.Troca(produto);
+
+        //    return Request.CreateResponse(HttpStatusCode.OK);
+        //}
+
+        [Route("api/carrinho/{idCarrinho}/produto/{idProduto}/quantidade")]
+        public HttpResponseMessage Put([FromBody]Produto produto, [FromUri] int idCarrinho, [FromUri] int idProduto)
         {
-            CarrinhoDAO dao = new CarrinhoDAO();
-            Carrinho carrinho = dao.Busca(id);
-            return carrinho;
+            var dao = new CarrinhoDAO();
+            var carrinho = dao.Busca(idCarrinho);
+
+            carrinho.TrocaQuantidade(produto);
+
+            return Request.CreateResponse(HttpStatusCode.OK);
         }
 
-        public string Post([FromBody]Carrinho carrinho)
+        [Route("api/carrinho/{idCarrinho}/produto/{idProduto}")]
+        public HttpResponseMessage Delete([FromUri] int idCarrinho, [FromUri] int idProduto)
+        {
+            CarrinhoDAO dao = new CarrinhoDAO();
+            Carrinho carrinho = dao.Busca(idCarrinho);
+            carrinho.Remove(idProduto);
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        public HttpResponseMessage Get(int id)
+        {
+            try
+            {
+                CarrinhoDAO dao = new CarrinhoDAO();
+                Carrinho carrinho = dao.Busca(id);
+                return Request.CreateResponse(HttpStatusCode.OK, carrinho); ;
+            }
+            catch (KeyNotFoundException)
+            {
+                string mensagem = string.Format("O carrinho {0} não foi encontrado", id);
+                HttpError error = new HttpError(mensagem);
+                return Request.CreateResponse(HttpStatusCode.NotFound, error);
+            }
+
+        }
+
+        public HttpResponseMessage Post([FromBody]Carrinho carrinho)
         {
             CarrinhoDAO dao = new CarrinhoDAO();
             dao.Adiciona(carrinho);
-            return "sucesso";
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created);
+
+            string location = Url.Link("DefaultApi", new { controller = "carrinho", id = carrinho.Id });
+            response.Headers.Location = new Uri(location);
+
+            return response;
         }
     }
 }
--- Alura.LojaAPIClient/Program.cs
+++ Alura.LojaAPIClient/Program.cs
@@ -9,12 +9,12 @@ namespace Alura.LojaAPIClient
     {
         static void Main(string[] args)
         {
-            TestaPost();
+            TestaPut();
         }
 
         static void TestaPost()
         {
-            string conteudo;
+
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:58891/api/carrinho");
             request.Method = "POST";
 
@@ -37,32 +37,74 @@ namespace Alura.LojaAPIClient
             byte[] jsonBytes = Encoding.UTF8.GetBytes(xml);
             request.GetRequestStream().Write(jsonBytes, 0, jsonBytes.Length);
 
-            request.ContentType = "application/json";
+            request.ContentType = "application/xml";
 
-            WebResponse response = request.GetResponse();
-            using (Stream responseStream = response.GetResponseStream())
-            {
-                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
-                conteudo = reader.ReadToEnd();
-            }
-            Console.Write(conteudo);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(response.Headers["Location"]);
             Console.Read();
+
         }
 
         static void TestaGet()
         {
             string conteudo;
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:58891/api/carrinho/1");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:58891/api/carrinho/2");
             request.Method = "GET";
             request.Accept = "application/json";
 
-            WebResponse response = request.GetResponse();
-            using (Stream responseStream = response.GetResponseStream())
-            {
-                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
-                conteudo = reader.ReadToEnd();
-            }
-            Console.Write(conteudo);
+            //WebResponse response = request.GetResponse();
+            //using (Stream responseStream = response.GetResponseStream())
+            //{
+            //    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+            //    conteudo = reader.ReadToEnd();
+            //}
+            //Console.Write(conteudo);
+            //Console.Read();
+
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(response.Headers["Location"]);
+            Console.Read();
+        }
+
+        static void TestaDelete()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:58891/api/carrinho/1/produto/3467");
+            request.Method = "DELETE";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine(response.StatusCode);
+
+            Console.Read();
+        }
+
+        static void TestaPut()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:58891/api/carrinho/1/produto/6237/quantidade");
+            request.Method = "PUT";
+
+            string xml = @"<Produto 
+                            xmlns:i=""http://www.w3.org/2001/XMLSchema-instance""
+                            xmlns = ""http://schemas.datacontract.org/2004/07/Alura.LojaAPI.Models"" >
+                                <Id>6237</Id>
+                                <Nome>Videogame de doido</Nome>
+                                <Preco>4000</Preco>
+                                <Quantidade> 100000 </Quantidade>
+                           </Produto>";
+            byte[] xmlBytes = Encoding.UTF8.GetBytes(xml);
+            request.GetRequestStream().Write(xmlBytes, 0, xmlBytes.Length);
+
+            request.ContentType = "Application/xml";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine(response.StatusCode);
+
             Console.Read();
         }
     }
